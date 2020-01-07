6c8c7504f910b2956263b79cd3272def917f7329
Iakov Taranenko <48700922+IakovTaranenko@users.noreply.github.com>
2020-01-06 22:09:40 -0800

    Update main.py
    Made sure that all requests will become capitalized and can pass through the alphabet

--- main.py
+++ main.py
@@ -45,23 +45,26 @@ prompt = imput('Would you like to encrypt your message or decrypt?')
 if (prompt.upper() == 'ENCRYPT'):
     request = input('Please enter the message you would like to encrypt')
     print('Your message is: ' + mask(request))
-else:
+'''else:
     request = input('Please enter the message you would like to decrypt')
     print('Your message is: ' + unmask(request))
+'''
 
 #Main function to mask message
 def mask(request):
     if (request and request != ''):
         message == ""
-        for character in request:
+		upperRequest = request.upper()
+        for character in upperRequest:
             if (character and character != ' '):
 				message += Alphabet[character] + ' '
-
+			else:
+				message
         return message
     else:
         return('ERROR')
         print('Message is invalid, please try again')
-
+'''
 #Main function to unmask message
 def unmask(request):
     if (request && request != ''):
@@ -75,3 +78,4 @@ def unmask(request):
         return('ERROR')
         print('Message is invalid, please try again')
 
+'''
